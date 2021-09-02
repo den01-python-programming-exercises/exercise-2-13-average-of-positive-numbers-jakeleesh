1bd4eeff13ff98ca59a82ea812cf247e2dc84568
Jake Lee <jake.lee@fdmgroup.com>
2021-09-02 12:42:06 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,19 @@
 def main():
     #write your code below this line
+    count = 0
+    sum = 0
+    while True:
+        num = int(input())
+        if (num > 0):
+            count += 1
+            sum += num
+        elif (num == 0):
+            break
+    
+    if(sum <= 0):
+        print("Cannot calculate the average")
+    else:
+        print(sum / count)
 
 if __name__ == '__main__':
     main()
